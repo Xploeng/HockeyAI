d7c832ea0175ee913836d339fdc5e30ceae937cc
Xploeng <julius@harald-rau.de>
2024-12-18 16:33:00 +0100

    continuous actions/observation

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,5 +1,8 @@
 import os
+import gymnasium as gym
 import torch
+
+from gymnasium import spaces
 
 
 def write_checkpoint(
@@ -26,3 +29,28 @@ def write_checkpoint(
         },
         f=dst_path,
     )
+
+
+class DiscreteActionWrapper(gym.ActionWrapper):
+    def __init__(self, env: gym.Env, bins=5):
+        """A wrapper for converting a 1D continuous actions into discrete ones.
+        Args:
+            env: The environment to apply the wrapper
+            bins: number of discrete actions
+        """
+        assert isinstance(env.action_space, spaces.Box)
+        super().__init__(env)
+        self.bins = bins
+        self.orig_action_space = env.action_space
+        self.action_space = spaces.Discrete(self.bins)
+
+    def action(self, action):
+        """discrete actions from low to high in 'bins'
+        Args:
+            action: The discrete action
+        Returns:
+            continuous action
+        """
+        return self.orig_action_space.low + action / (self.bins - 1.0) * (
+            self.orig_action_space.high - self.orig_action_space.low
+        )
